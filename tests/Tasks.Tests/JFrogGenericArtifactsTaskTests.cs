74e6cf1ca60cecccfdef2eee730689f331668a5c
David Gardiner <david@gardiner.net.au>
2025-01-11 16:32:56 +1030

    Add more properties to Upload

--- tests/Tasks.Tests/JFrogGenericArtifactsTaskTests.cs
+++ tests/Tasks.Tests/JFrogGenericArtifactsTaskTests.cs
@@ -21,8 +21,35 @@ public class JfrogGenericArtifactTaskTests
     [Fact]
     public Task Upload_Command()
     {
+        var task = _builder.Upload("serviceConnection");
+
+        return Verify(GetYaml(task));
+    }
+
+    [Fact]
+    public Task Upload_Full_Command()
+    {
         var task = _builder.Upload("serviceConnection") with
         {
+            CollectBuildInfo = true,
+            SpecSource = SpecSources.TaskConfiguration,
+            FileSpec = """
+                       {
+                               "files": [
+                                 {
+                                   "pattern": "libs-generic-local/*.zip",
+                                   "target": "dependencies/files/"
+                                 }
+                               ]
+                             }
+                       """,
+            BuildName = "ThisBuild",
+            BuildNumber = "1.0.0",
+            ReplaceSpecVars = true,
+            SpecVars = "key1=value1;key2=value2",
+            ProjectKey = "projectKey",
+            IncludeEnvVars = true,
+            FailNoOp = true,
             WorkingDirectory = Path.GetTempPath()
         };
 
